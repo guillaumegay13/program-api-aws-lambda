f4b121da4b8388366fc35ab6eb31c59d35d0614a
Guillaume Gay <ggay@dalet.com>
2024-01-20 19:48:15 +0100

    merged code refactoring adapted to chalice

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from chalice import Chalice
-from chalicelib.workflow import Workflow
+from chalicelib.api import TrainProgramApi
 from langchain_openai import ChatOpenAI
 from pydantic import BaseModel
 import logging
+
+app = Chalice(app_name='aws-lambda-deployment')
+app.log.setLevel(logging.DEBUG)
 
 gptJsonModel = ChatOpenAI(
     #models : https://platform.openai.com/docs/models/gpt-3-5
@@ -17,7 +20,7 @@ gptJsonModel = ChatOpenAI(
 )
 
 # Define a Pydantic model for the JSON data input
-class ProgramInput(BaseModel):
+class UserInput(BaseModel):
     type: str
     gender: str
     level: str
@@ -27,16 +30,60 @@ class ProgramInput(BaseModel):
     weight: int
     age: int
 
-app = Chalice(app_name='aws-lambda-deployment')
-app.log.setLevel(logging.DEBUG)
+# Need all user input plus evidences as dict
+class MethodsInput(UserInput):
+    evidences: dict
 
+# Need all user input plus methods as dict
+class ProgramInput(UserInput):
+    methods: dict
+
+@app.route('/provides_evidences', methods=['POST'], content_types=['application/json'])
+async def provide_evidences():
+    request = app.current_request
+    userInput = UserInput(**request.json_body)
+    trainProgramApi = TrainProgramApi(gptJsonModel, userInput.dict())
+    evidences = trainProgramApi.provide_evidences()
+    return evidences
+
+@app.route('/generate_methods', methods=['POST'], content_types=['application/json'])
+async def generate_methods():
+    request = app.current_request
+    methodsInput = MethodsInput(**request.json_body)
+    trainProgramApi = TrainProgramApi(gptJsonModel, methodsInput.dict())
+    evidences = trainProgramApi.provide_evidences()
+    methods = trainProgramApi.generate_methods(evidences)
+    return methods
+
+@app.route('/generate_program', methods=['POST'], content_types=['application/json'])
+async def generate_program(input_data: ProgramInput):
+    request = app.current_request
+    programInput = ProgramInput(**request.json_body)
+    trainProgramApi = TrainProgramApi(gptJsonModel, programInput.dict())
+    evidences = trainProgramApi.provide_evidences()
+    methods = trainProgramApi.generate_methods(evidences)
+    program = trainProgramApi.generate_program(methods)
+    return program
+
+# For the full workflow, only the User Input is needed
+@app.route('/generate_train_program', methods=['POST'], content_types=['application/json'])
+async def generate_train_program():
+    request = app.current_request
+    userInput = UserInput(**request.json_body)
+    trainProgramApi = TrainProgramApi(gptJsonModel, userInput.dict())
+    # Run the end-to-end workflow
+    # TODO : write the workflow code below instead of a class method
+    trainProgramApi.run_workflow()
+
+# Ping GET endpoint for testing purposes
 @app.route('/ping')
 def index():
     return {'hello': 'world'}
 
+# Response time is too long, split the workflow in multiple endpoints
 @app.route('/program', methods=['POST'], content_types=['application/json'])
 def create_program():
     request = app.current_request
     instance = ProgramInput(**request.json_body)
-    result = Workflow(gptJsonModel, instance.dict())
+    result = TrainProgramApi(gptJsonModel, instance.dict())
     return result
